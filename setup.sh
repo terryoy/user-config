34919a9eae746bfd65088fd1da28c16627453c31
teatime@lucki.cn <teatime@lucki.cn>
2020-05-25 12:17:49 +0800

    fix: setup script support https instead of ssh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 set -e
 
 cd ~
-git clone git@github.com:terryoy/user-config.git
+git clone https://github.com/terryoy/user-config.git
+echo "you can change repository's origin to git@github.com/terryoy/user-config.git"
 
 cd user-config
 chmod +x env_setup.sh
