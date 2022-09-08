0315b8a824bc948e301d3d16ab1ddf4771f0d190
terryoy <terry.ouyang@gmail.com>
2022-09-08 15:15:24 +0800

    fix: python init

--- alias/python.sh
+++ alias/python.sh
@@ -7,11 +7,13 @@ alias pip="pip3"
 # Python Virtual Environment
 export VIRTUALENVWRAPPER_PYTHON=`which python3`
 export WORKON_HOME=~/.venv
-VENV_SCRIPT=`which virtualenvwrapper.sh`
+VENV_SCRIPT=~/.local/bin/virtualenvwrapper.sh 
 
-[ -e $VENV_SCRIPT ] || VENV_SCRIPT='~/.local/bin/virtualenvwrapper.sh'
+[ -e $VENV_SCRIPT ] || VENV_SCRIPT=`which virtualenvwrapper.sh`
+
 if [[ -f $VENV_SCRIPT ]]
 then
+    echo "init python virtualenv wrapper"
     source $VENV_SCRIPT
 fi
 
