ea7bf1c199978a4c9f4c5cf0490efd0221f196f1
terryoy <terry.ouyang@gmail.com>
2022-11-13 10:43:31 +0800

    fix: golang script

--- alias/golang.sh
+++ alias/golang.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+
 # run an image with a temporary container
 function run_go() {
 
+  export WORK_DIR="${PWD}/${1}"
+
   if [[ -n "$1" ]]; then
+	echo "start go environment with working dir: $WORK_DIR -> /go/src/terryoy/$1"
   	docker run --rm -it --name go-env \
-  		-v $1:/go/src/terryoy/$1 golang
+  		-v $WORK_DIR:/go/src/terryoy/$1 golang
   else
 	echo "run_go [path]"
   fi
