236e461396c254ae9953d35abb543257f65a87bb
terryoy <terry.ouyang@gmail.com>
2022-11-13 10:27:07 +0800

    fix: golang

--- alias/golang.sh
+++ alias/golang.sh
@@ -3,7 +3,7 @@
 # run an image with a temporary container
 function run_go() {
 
-  if [[ -n "$1 ]]; then
+  if [[ -n "$1" ]]; then
   	docker run --rm -it --name go-env \
   		-v $1:/go/src/terryoy/$1 golang
   else
